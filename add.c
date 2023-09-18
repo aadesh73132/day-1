83221a0ebc6bdafbdca9d76ebd73250c484a3d18
aadesh73132 <145355002+aadesh73132@users.noreply.github.com>
2023-09-18 20:04:35 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int T;
+cin >> T;
+while(T--)
+{
+    int X;
+    cin >> X;
+    if(X>= 2000){
+
+        cout<<"yes"<<end1;
+    }
+    else
+    {
+        cout<<"no"<<end1;
+    }
+
+}
+return 0;
+
+}
